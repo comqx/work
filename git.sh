01b34d689c3efdb9bd39269788626dfa25bc0331
liuk-l <liuk-l@glodon.com>
2019-04-11 17:31:31 +0800

    'master_20190411:1731'

--- git.sh
+++ git.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
-#cat << EOF >  ~/.git-credentials
-#https://18311166263\@163.com:liu890892@github.com
-#http://18311166263\@163.com:liu890892@github.com
-#EOF
+cat << EOF >  ~/.git-credentials
+https://18311166263\@163.com:liu890892@github.com
+http://18311166263\@163.com:liu890892@github.com
+EOF
 
 timer=`date +%Y%m%d:%H%M`
 git add --all 
 git commit -m "'master_$timer'"
+git pull origin dingding
 git remote add origin https://github.com/six-lqx/work.git
 git config --global credential.helper store 
 git push origin dingding
